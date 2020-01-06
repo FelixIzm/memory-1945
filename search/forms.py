9faebfe370eb9284cb21aa0ea22bb6cd02883170
FelixIzm <for_cv@mail.ru>
2020-01-06 17:11:13 +0300

    heroku 06.01.2019 17:11

--- search/forms.py
+++ search/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from datetime import date
-from django.utils import timezone
 
 class UserForm(forms.Form):
     name = forms.CharField()
     age = forms.IntegerField()
-    dateFrom = forms.DateField(default=timezone.now)
+    dateFrom = forms.DateField(initial=date.today)
 
