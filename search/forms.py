b0dbacd6b4d787345440fa4fb1f33ba761ca1ac6
FelixIzm <for_cv@mail.ru>
2020-01-06 17:29:33 +0300

    heroku 06.01.2019 17:29

--- search/forms.py
+++ search/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from datetime import date
 
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
-    date_From = forms.DateField(initial=date.today, widget=forms.widgets.DateInput(format="%d.%m.%Y"))
-    date_To = forms.DateField(initial=date.today, widget=forms.widgets.DateInput(format="%d.%m.%Y"))
+    d_from = "01-01-1939"
+    d_to = "01-01-1946"
+    unit = forms.CharField(label='Воинское подразделение')
+    date_From = forms.DateField(initial=datetime.datetime.strptime(d_from, "%d-%m-%Y").date(), widget=forms.widgets.DateInput(format="%d.%m.%Y"))
+    date_To = forms.DateField(initial=datetime.datetime.strptime(d_to, "%d-%m-%Y").date(), widget=forms.widgets.DateInput(format="%d.%m.%Y"))
 
