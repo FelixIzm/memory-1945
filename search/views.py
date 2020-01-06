3c2da7a2c126d46a7fb229108aca939072594045
FelixIzm <for_cv@mail.ru>
2020-01-06 17:49:12 +0300

    heroku 06.01.2019 17:49

--- search/views.py
+++ search/views.py
@@ -171,8 +171,8 @@ def getContent(military_unit):
 def index(request):
     if request.method == "POST":
         unit = request.POST.get("unit")
-        #date_From = request.POST.get("date_From")
-        #date_To = request.POST.get("date_To")
+        date_From = request.POST.get("date_From")
+        date_To = request.POST.get("date_To")
         # age = request.POST.get("age") # получение значения поля age
         #return HttpResponse("<h2>Hello, {0}</h2>".format(name))
         return HttpResponse(getContent(unit))
