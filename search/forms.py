e971b630bdb617f4de5ae75446d2a47f4b45b2e2
FelixIzm <for_cv@mail.ru>
2020-01-06 16:07:00 +0300

    heroku 06.01.2019 16:05

--- search/forms.py
+++ search/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField()
     age1 = forms.IntegerField()
-    dateFrom = forms.DateField()
+    #dateFrom = forms.DateField()
