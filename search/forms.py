93a4c212ed8af53e3a6a8205419c4e30c2d1b75f
FelixIzm <for_cv@mail.ru>
2020-01-06 16:59:11 +0300

    heroku 06.01.2019 16:59

--- search/forms.py
+++ search/forms.py
@@ -4,5 +4,5 @@ from datetime import date
 class UserForm(forms.Form):
     name = forms.CharField()
     age = forms.IntegerField()
-    dateFrom = forms.DateField(auto_now=True, auto_now_add=True, widget=forms.widgets.DateInput(format="%m/%d/%Y"))
+    dateFrom = forms.DateField(auto_now=False, auto_now_add=False, widget=forms.widgets.DateInput(format="%m/%d/%Y"))
 
