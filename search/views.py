0f333b486163a085af8ce865b364fc3b05e09fd9
FelixIzm <for_cv@mail.ru>
2020-01-07 06:14:49 +0200

    heroku 07.01.2019 07:14

--- search/views.py
+++ search/views.py
@@ -143,7 +143,7 @@ def getContent(military_unit, date_From, date_To):
                             #print(hit['_source'])
                             src = hit['_source']
                             data_string = table_string.safe_substitute(col1=src['document_type'],col2=src['document_name'],col3=src['date_from']+'-'+src['date_to'],col4=src['authors'],col5=src['document_date_f'],col6=src['archive'],col7=src['fond'],col8=src['opis'],col9=src['delo'],
-                            col10='<a href=https://pamyat-naroda.ru/documents/view/?id='+hit['_id']+'>Док</a>')
+                            col10='<a href=https://pamyat-naroda.ru/documents/view/?id='+hit['_id']+' target="_blank">Док</a>')
                             #col10='<a href=https://cdn.pamyat-naroda.ru/imageloadfull/'+src['image_path']+'>Скан</a>')
                             #"https://pamyat-naroda.ru/documents/view/?id=156069047
                             #f.write(data_string)
@@ -159,7 +159,7 @@ def getContent(military_unit, date_From, date_To):
                     for hit in hits:
                         src = hit['_source']
                         data_string = table_string.safe_substitute(col1=src['document_type'],col2=src['document_name'],col3=src['date_from']+'-'+src['date_to'],col4=src['authors'],col5=src['document_date_f'],col6=src['archive'],col7=src['fond'],col8=src['opis'],col9=src['delo'],
-                        col10='<a href=https://pamyat-naroda.ru/documents/view/?id='+hit['_id']+'>Док</a>')
+                        col10='<a href=https://pamyat-naroda.ru/documents/view/?id='+hit['_id']+' target="_blank">Док</a>')
                         #col10='<a href=https://cdn.pamyat-naroda.ru/imageloadfull/'+src['image_path']+'>Скан</a>')
                         #col10='<input value="scan" onclick="window.open(\'https://cdn.pamyat-naroda.ru/imageloadfull/'+ src['image_path']+'\')" type="button">')
 
