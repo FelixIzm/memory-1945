602b156fd5be73233d63ed307ff5d9302dd41b5f
FelixIzm <for_cv@mail.ru>
2020-01-08 00:08:13 +0200

    00:58

--- search/urls.py
+++ search/urls.py
@@ -5,6 +5,5 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     #url(r'^favicon\.ico$',RedirectView.as_view(url='/static/favicon.ico'), name='favicon'),
-    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
     path('', views.index, name='index'),
 ]
