59d5f0f98389f304440a375814393fd56305903a
FelixIzm <for_cv@mail.ru>
2020-02-03 16:40:06 +0300

    '03-02'

--- search/views.py
+++ search/views.py
@@ -177,20 +177,11 @@ def getContent(military_unit, date_From, date_To):
                     for hit in hits:
                         src = hit['_source']
                         print("src = ",src)
-                        try:
-                            data_string = table_string.safe_substitute(col1=src['document_type'],col2=src['document_name'],col3=src['date_from']+'-'+src['date_to'],col4=src['authors'],col5=src['document_date_f'],col6=src['archive'],col7=src['fond'],col8=src['opis'],col9=src['delo'],col10='<a href=https://pamyat-naroda.ru/documents/view/?id='+hit['_id']+' target="_blank">Док</a>')
-                        except ValueError as e:
-                            print("data_string = ",e)
+                        data_string = table_string.safe_substitute(col1=src['document_type'],col2=src['document_name'],col3=src['date_from']+'-'+src['date_to'],col4=src['authors'],col5=src['document_date_f'],col6=src['archive'],col7=src['fond'],col8=src['opis'],col9=src['delo'],col10='<a href=https://pamyat-naroda.ru/documents/view/?id='+hit['_id']+' target="_blank">Док</a>')
                             
-                        #col10='<a href=https://cdn.pamyat-naroda.ru/imageloadfull/'+src['image_path']+'>Скан</a>')
-                        #col10='<input value="scan" onclick="window.open(\'https://cdn.pamyat-naroda.ru/imageloadfull/'+ src['image_path']+'\')" type="button">')
-
-                        #f.write(data_string)
-                        try:
-                            html_string += data_string
-                        except ValueError as e:
-                            print('data_string = ',data_string)
-                            print('html_string = ',e)
+                        html_string += data_string
+                        print('data_string = ',data_string)
+                        print('html_string = ',html_string)
                 print('count = ',search_count)
                 #f.write('</table></html>')
                 html_string+='</tbody></table></html>'
