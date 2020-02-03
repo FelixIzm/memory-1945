f739266f8d41a365859580cbda90ff9a5e57bb5e
FelixIzm <for_cv@mail.ru>
2020-02-03 18:42:27 +0300

    '03-02'

--- search/views.py
+++ search/views.py
@@ -149,6 +149,8 @@ def getContent(military_unit, date_From, date_To):
                     for hit in hits:
                         src = hit['_source']
                         data_string = table_string.safe_substitute(col1=src['document_type'],col2=src['document_name'],col3=src['date_from']+'-'+src['date_to'],col4=src['authors'],col5=src['document_date_f'],col6=src['archive'],col7=src['fond'],col8=src['opis'],col9=src['delo'],col10='<a href=https://pamyat-naroda.ru/documents/view/?id='+hit['_id']+' target="_blank">Док</a>')
+                        html_string += data_string
+                        
                 #f.write('</table></html>')
                 html_string+='</tbody></table></html>'
                 #f.close()
